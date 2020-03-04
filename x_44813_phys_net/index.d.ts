bc6c98ca26aca2ee87b127a3eace316796b962af
erwinel <leonard.t.erwine.ctr@mail.mil>
2020-03-04 15:05:30 -0500

    Added type definitions for x_44813_phys_net, x_44813_sec_clsif_definition and x_44813_usasoc_cst

---
+++ x_44813_phys_net/index.d.ts
@@ -0,0 +1,10 @@
+/// <reference path="../index.d.ts" />
+
+/**
+ * GlideElement values from the Physical Network table.
+ * @interface x_44813_phys_net_networkFields
+ * @extends {cmdb_ciFields}
+ */
+declare interface x_44813_phys_net_networkFields extends cmdb_ciFields {
+}
+declare type x_44813_phys_net_networkGlideRecord = cmdb_ciGlideRecord & x_44813_phys_net_networkFields;
